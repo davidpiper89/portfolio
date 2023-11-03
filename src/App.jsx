26bf7e00ce11bcd1e810f9bd847338d5cfc9d420
davidpiper89 <davidpiper1989@gmail.com>
2023-11-03 09:14:42 +0000

    created components folder and begun creating intro animation component using GSAP.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import IntroAnimation from "./components/IntroAnimation";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <IntroAnimation />
+    </div>
+  );
+}
+
+export default App;
